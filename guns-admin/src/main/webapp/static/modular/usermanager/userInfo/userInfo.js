ace47e064e0c6a339a5b5a050c4ab175af70eb85
wuxiaochun <apple1981@126.com>
2018-03-07 19:06:48 +0800

    临时提交

---
+++ guns-admin/src/main/webapp/static/modular/usermanager/userInfo/userInfo.js
@@ -0,0 +1,106 @@
+/**
+ * 接口权限管理管理初始化
+ */
+var UserInfo = {
+    id: "UserInfoTable",	//表格id
+    seItem: null,		//选中的条目
+    table: null,
+    layerIndex: -1
+};
+
+/**
+ * 初始化表格的列
+ */
+UserInfo.initColumn = function () {
+    return [
+        {field: 'selectItem', radio: true},
+            {title: '', field: 'id', visible: true, align: 'center', valign: 'middle'},
+            {title: '用户名', field: 'userName', visible: true, align: 'center', valign: 'middle'},
+            {title: '手机号', field: 'phone', visible: true, align: 'center', valign: 'middle'},
+            {title: '应用id', field: 'appid', visible: true, align: 'center', valign: 'middle'},
+            {title: '用户登录token', field: 'token', visible: true, align: 'center', valign: 'middle'},
+            {title: '成功访问数量', field: 'accessCount', visible: true, align: 'center', valign: 'middle'},
+            {title: '失败访问次数', field: 'failCount', visible: true, align: 'center', valign: 'middle'},
+            {title: '状态 0 正常 1 暂停 2 删除', field: 'status', visible: true, align: 'center', valign: 'middle'},
+            {title: '创建人', field: 'createUser', visible: true, align: 'center', valign: 'middle'},
+            {title: '创建时间', field: 'createTime', visible: true, align: 'center', valign: 'middle'}
+    ];
+};
+
+/**
+ * 检查是否选中
+ */
+UserInfo.check = function () {
+    var selected = $('#' + this.id).bootstrapTable('getSelections');
+    if(selected.length == 0){
+        Feng.info("请先选中表格中的某一记录！");
+        return false;
+    }else{
+        UserInfo.seItem = selected[0];
+        return true;
+    }
+};
+
+/**
+ * 点击添加接口权限管理
+ */
+UserInfo.openAddUserInfo = function () {
+    var index = layer.open({
+        type: 2,
+        title: '添加接口权限管理',
+        area: ['800px', '420px'], //宽高
+        fix: false, //不固定
+        maxmin: true,
+        content: Feng.ctxPath + '/userInfo/userInfo_add'
+    });
+    this.layerIndex = index;
+};
+
+/**
+ * 打开查看接口权限管理详情
+ */
+UserInfo.openUserInfoDetail = function () {
+    if (this.check()) {
+        var index = layer.open({
+            type: 2,
+            title: '接口权限管理详情',
+            area: ['800px', '420px'], //宽高
+            fix: false, //不固定
+            maxmin: true,
+            content: Feng.ctxPath + '/userInfo/userInfo_update/' + UserInfo.seItem.id
+        });
+        this.layerIndex = index;
+    }
+};
+
+/**
+ * 删除接口权限管理
+ */
+UserInfo.delete = function () {
+    if (this.check()) {
+        var ajax = new $ax(Feng.ctxPath + "/userInfo/delete", function (data) {
+            Feng.success("删除成功!");
+            UserInfo.table.refresh();
+        }, function (data) {
+            Feng.error("删除失败!" + data.responseJSON.message + "!");
+        });
+        ajax.set("userInfoId",this.seItem.id);
+        ajax.start();
+    }
+};
+
+/**
+ * 查询接口权限管理列表
+ */
+UserInfo.search = function () {
+    var queryData = {};
+    queryData['condition'] = $("#condition").val();
+    UserInfo.table.refresh({query: queryData});
+};
+
+$(function () {
+    var defaultColunms = UserInfo.initColumn();
+    var table = new BSTable(UserInfo.id, "/userInfo/list", defaultColunms);
+    table.setPaginationType("client");
+    UserInfo.table = table.init();
+});
---
+++ guns-rest/src/main/java/com/stylefeng/guns/rest/config/Constant.java
@@ -0,0 +1,72 @@
+package com.stylefeng.guns.rest.config;
+
+/**
+ * com.stylefeng.guns.rest.config
+ * <p>
+ * <p>
+ * Copyright: Copyright (c) 2018/3/5 13:46
+ * <p>
+ * Company: 京东
+ * <p>
+ *
+ * @author wuxiaochun@jd.com
+ * @version 1.0.0
+ */
+public class Constant {
+
+    /**
+     * 请求类型
+     */
+    public interface Query {
+        /** 多借贷 */
+        String duotou = "duotou";
+        /** 金融逾期 */
+        String jinrong = "jinrong";
+        /** 犯罪信息 */
+        String fanzui = "fanzui";
+        /** 高法失信 */
+        String gaofa = "gaofa";
+    }
+
+    /**
+     * 返回结果类型
+     */
+    public interface ResponseCode {
+        /** 成功 */
+        String ok = "200";
+        /** 余额不足 */
+        String insufficientBalance = "403";
+        /** 尚未授权 */
+        String unauthorized = "405";
+        /** 用户不存在 */
+        String nonExistent = "408";
+        /** 校验不通过/请求失败 */
+        String validationNot = "400";
+        /** 内部错误 */
+        String systemError = "500";
+        /** token过期 */
+        String tokenTimeOut = "700";
+    }
+
+    /**
+     * 返回结果类型
+     */
+    public interface ResponseMsg {
+        /** 成功 */
+        String ok = " 成功";
+        /** 余额不足 */
+        String insufficientBalance = "余额不足";
+        /** 尚未授权 */
+        String unauthorized = "尚未授权";
+        /** 用户不存在 */
+        String nonExistent = "用户不存在";
+        /** 校验不通过/请求失败 */
+        String validationNot = "校验不通过/请求失败";
+        /** 内部错误 */
+        String systemError = "系统错误请联系管理员";
+        /** token过期 */
+        String tokenTimeOut = "token过期";
+
+    }
+
+}
